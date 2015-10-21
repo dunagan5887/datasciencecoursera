5d61e542ba146f807a9984d06fb7ed59b2941bd8
Sean Dunagan <dunagan5887@gmail.com>
2015-10-21 15:45:40 -0500

    Updating file which reads data to fetch file from remote

--- exploratory_data_analysis/course_project_2/read_summary_data.R
+++ exploratory_data_analysis/course_project_2/read_summary_data.R
@@ -1,12 +1,34 @@
+library("downloader");
+
 pmSummaryFactory <- function()
 {
+    remote_file_location <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip";
+    zipped_data_file_name <- 'NEI_data.zip';
+    data_has_been_downloaded <- FALSE;
+    
+    summary_data_filename <- "summarySCC_PM25.rds";
     summaryDataFrame <- NULL;
+    source_classification_codes_filename <- "Source_Classification_Code.rds";
     sourceClassificationCodes <- NULL;
     
     readInData <- function()
     {
-        summaryDataFrame <<- readRDS("summarySCC_PM25.rds");
-        sourceClassificationCodes <<- readRDS("Source_Classification_Code.rds");
+        if (!data_has_been_downloaded)
+        {
+            download(remote_file_location, dest=zipped_data_file_name, mode="wb");
+            unzip (zipped_data_file_name, exdir = "./")
+            data_has_been_downloaded <<- TRUE;
+        }
+        
+        if (is.null(summaryDataFrame))
+        {
+            summaryDataFrame <<- readRDS(summary_data_filename);
+        }
+        
+        if(is.null(sourceClassificationCodes))
+        {
+            sourceClassificationCodes <<- readRDS(source_classification_codes_filename);
+        }
     }
     
     getSummaryDataFrame <- function()
@@ -26,7 +48,11 @@ pmSummaryFactory <- function()
     )
 }
 
-pmSummaryInstance <- pmSummaryFactory();
+if (!exists("pmSummaryInstance"))
+{
+    pmSummaryInstance <- pmSummaryFactory();
+}
+
 pmSummaryInstance$readInData();
 summaryData <- pmSummaryInstance$getSummaryDataFrame();
-
+sourceClassifications <- pmSummaryInstance$getSourceClassificationCodes();
