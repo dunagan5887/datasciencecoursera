24df39e9f7f2830010d0bc00af4bad99346267bf
Sean Dunagan <dunagan5887@gmail.com>
2015-10-23 17:15:00 -0500

    Initial draft of Regression Models Course Project

--- regression_models_course_project/course_project.R
+++ regression_models_course_project/course_project.R
@@ -40,41 +40,10 @@ mtcarsRegressionAnalyzerFactory <- function()
     {
         par(mfrow=c(1,1), oma=c(0,2,2,0))
         boxplot(mpg ~ am, data = mtcars, col=c("Blue", "Red"), range=0, boxwex = 0.4,
-                ylab="MPG", main = "MPG by Transmission Type", xlab="Transmission Type",
+                ylab="MPG", main = "Figure 1\nMPG by Transmission Type", xlab="Transmission Type",
                 names=c("Automatic", "Manual"))
         legend("topleft", col=c("Blue", "Red"), legend = c("Automatic", "Manual"),
                lty=c(1,1,1), lwd=c(2.5,2.5,2.5))
-        #dev.copy(png, file="mpg_by_transmission_boxplot.png")
-        #dev.off();
-    }
-    
-    historgramMpgByTransmissions <- function()
-    {
-        par(mfrow=c(2,1), oma=c(0,2,2,0))
-        with(mtcars, {
-            hist(automaticCarsDataFrame$mpg, xlim=c((min_mpg_value - 1), (max_mpg_value + 1)), ylim=c(0,3), 
-                 breaks=(min_mpg_value - 1):(max_mpg_value + 1),
-                 col="Black", border="White", xlab="MPG", main="Automatic Transmission",
-                 ylab = "Number Of Cars")
-            hist(manualCarsDataFrame$mpg, xlim=c((min_mpg_value - 1), (max_mpg_value + 1)), ylim=c(0,3), 
-                 breaks=(min_mpg_value - 1):(max_mpg_value + 1),
-                 col="Black", border="White", xlab="MPG", main="Manual Transmission",
-                 ylab = "Number Of Cars")
-            mtext("MPG by Transmission Types", outer=TRUE)
-        })
-        
-        #dev.copy(png, file="mpg_by_transmission_histograms.png")
-        #dev.off();
-    }
-    
-    executeTIntervalTest <- function()
-    {
-        separateAutomaticAndManualTransmissionCars();
-        automaticCarsDataFrame <- mtcarsRegressionAnalyzerInstance$getAutomaticCars();
-        manualCarsDataFrame <- mtcarsRegressionAnalyzerInstance$getManualCars();
-        result <- t.test(manualCarsDataFrame$mpg, automaticCarsDataFrame$mpg, paired=FALSE,
-                         var.equal=FALSE)
-        result
     }
     
     summarizeMpgByManualTransmission <- function()
@@ -86,66 +55,32 @@ mtcarsRegressionAnalyzerFactory <- function()
     {
         summary(automaticCarsDataFrame$mpg);
     }
-    
-    plotLinearModelBetweenMpgAndTransmissionUsingBase <- function()
-    {
-        linearModel <- lm(mtcars$mpg ~ mtcars$am);
-        mean_automatic_mpg <- mean(automaticCarsDataFrame$mpg)
-        mean_manual_mpg <- mean(manualCarsDataFrame$mpg)
-        mean_mpg <- c(mean_automatic_mpg, mean_manual_mpg)
-        tranmission_types <- c(0,1)
-        
-        par(mfrow=c(1,1), oma=c(0,2,2,0))
-        with(mtcars, {
-            plot(automaticCarsDataFrame$am, automaticCarsDataFrame$mpg, pch=17,
-                 ylim=c((min_mpg_value - 1), (max_mpg_value + 1)), col="Blue",
-                 xlim=c(-.5,1.5), ylab="MPG", xlab="Transmission Type", 
-                 main="Linear Relationship between\nMPG and Transmission Type")
-            points(manualCarsDataFrame$am, manualCarsDataFrame$mpg, pch=17, col="Red")
-            abline(linearModel, lwd=2)
-            abline(h=mean_automatic_mpg, lwd=1, col="Blue")
-            abline(h=mean_manual_mpg, lwd=1, col="Red")
-            legend("topleft", col=c("Black", "Blue", "Red"),
-                   legend=c("Linear Relationship Fit", "Automatic Empirical Mean", "Manual Empirical Mean"),
-                   lty=c(1,1,1), lwd=c(2.5,2.5,2.5), cex=.85, bty="n")
-            legend("bottomright", col=c("Blue", "Red"), 
-                   legend=c("Automatic MPG Values", "Manual MPG Values"), pch=c(17,17), 
-                   cex=.75, bty="n")
-        })
-        
-        #dev.copy(png, file="linear_regression_plot_using_base.png")
-        #dev.off();
-    }
-    
-    plotLinearModelBetweenMpgAndTransmissionUsingGgplot <- function()
-    {
-        linearModel <- lm(mtcars$mpg ~ mtcars$am);
-        mean_automatic_mpg <- mean(automaticCarsDataFrame$mpg)
-        mean_manual_mpg <- mean(manualCarsDataFrame$mpg)
-        mean_mpg <- c(mean_automatic_mpg, mean_manual_mpg)
-        tranmission_types <- c(0,1)
-        
-        g <- ggplot(mtcars, aes(am, mpg))
-        g <- g + geom_point()
-        g <- g + geom_smooth(method = "lm")
-        g <- g + labs(title = "MtCars MPG by Transmission Type") + labs(x = "Is Manual Transmission Type", y = "MPG")
-        print(g)
-        
-        dev.copy(png, file="linear_regression_plot_using_ggplot.png")
-        dev.off();
-    }
-    
+
     plotResidualsBetweenMpgAndTransmission <- function()
     {
-        linearModel <- lm(mtcars$mpg ~ mtcars$am);
-        residuals <- resid(linearModel);
-        mtcars$residuals <- residuals;
+        amAgainstWeightCylinderCarbs <- lm(am ~ wt + cyl + carb, data = mtcars);
+        eAmAgainstWeightCylinderCarbs <- resid(amAgainstWeightCylinderCarbs);
+        mpgAgainstWeightCylinderCarbs <- lm(mpg ~ wt + cyl + carb, data = mtcars);
+        eMpgAgainstWeightCylinderCarbs <- resid(mpgAgainstWeightCylinderCarbs);
         
-        g <- ggplot(mtcars, aes(am, residuals))
+        g <- ggplot(mtcars, aes(x = eAmAgainstWeightCylinderCarbs, y = eMpgAgainstWeightCylinderCarbs))
         g <- g + geom_point(aes(color = am), size = 2);
-        g <- g + geom_smooth(size = 3, method = "lm", se = TRUE)
-        g <- g + coord_cartesian(xlim = c(-.25, 1.25));
+        g <- g + geom_smooth(size = 3, method = "lm", se = TRUE);
+        g <- g + labs(title="Residuals Plot To Show relationship\nbetween MPG and Transmission Type");
+        g <- g + labs(x="Residuals of Transmission Type\nagainst Weight, Cylinders and Carburetors");
+        g <- g + labs(y="Residuals of MPG\nagainst Weight, Cylinders and Carburetors");
         print(g)
+    }
+    
+    getModelResidualsOfMpgAgainstAm <- function()
+    {
+        amAgainstWeightCylinderCarbs <- lm(am ~ wt + cyl + carb, data = mtcars);
+        eAmAgainstWeightCylinderCarbs <- resid(amAgainstWeightCylinderCarbs);
+        mpgAgainstWeightCylinderCarbs <- lm(mpg ~ wt + cyl + carb, data = mtcars);
+        eMpgAgainstWeightCylinderCarbs <- resid(mpgAgainstWeightCylinderCarbs);
+        
+        residualMpgAgainstResidualAm <- lm(eMpgAgainstWeightCylinderCarbs ~ eAmAgainstWeightCylinderCarbs);
+        residualMpgAgainstResidualAm
     }
     
     list (separateAutomaticAndManualTransmissionCars = separateAutomaticAndManualTransmissionCars,
@@ -154,10 +89,8 @@ mtcarsRegressionAnalyzerFactory <- function()
           boxplotMpgByTransmissions = boxplotMpgByTransmissions,
           summarizeMpgByManualTransmission = summarizeMpgByManualTransmission,
           summarizeMpgByAutomaticTransmission = summarizeMpgByAutomaticTransmission,
-          historgramMpgByTransmissions = historgramMpgByTransmissions,
-          plotLinearModelBetweenMpgAndTransmissionUsingBase = plotLinearModelBetweenMpgAndTransmissionUsingBase,
-          plotLinearModelBetweenMpgAndTransmissionUsingGgplot = plotLinearModelBetweenMpgAndTransmissionUsingGgplot,
-          plotResidualsBetweenMpgAndTransmission = plotResidualsBetweenMpgAndTransmission)
+          plotResidualsBetweenMpgAndTransmission = plotResidualsBetweenMpgAndTransmission,
+          getModelResidualsOfMpgAgainstAm = getModelResidualsOfMpgAgainstAm)
 }
 
 createBoxPlot <- function()
@@ -168,41 +101,9 @@ createBoxPlot <- function()
     mtcarsRegressionAnalyzerInstance$historgramMpgByTransmissions();
 }
 
-createHistogramPlot <- function()
-{
-    mtcarsRegressionAnalyzerInstance <- mtcarsRegressionAnalyzerFactory();
-    mtcarsRegressionAnalyzerInstance$separateAutomaticAndManualTransmissionCars();
-    mtcarsRegressionAnalyzerInstance$historgramMpgByTransmissions();
-}
-
-testTInterval <- function()
-{
-    mtcarsRegressionAnalyzerInstance <- mtcarsRegressionAnalyzerFactory();
-    mtcarsRegressionAnalyzerInstance$separateAutomaticAndManualTransmissionCars();
-    automaticCarsDataFrame <- mtcarsRegressionAnalyzerInstance$getAutomaticCars();
-    manualCarsDataFrame <- mtcarsRegressionAnalyzerInstance$getManualCars();
-    
-    result <- t.test(manualCarsDataFrame$mpg, automaticCarsDataFrame$mpg, paired=FALSE,
-                var.equal=FALSE)
-    
-    result
-}
-
-testLinearModelPlot <- function()
-{
-    mtcarsRegressionAnalyzerInstance <- mtcarsRegressionAnalyzerFactory();
-    mtcarsRegressionAnalyzerInstance$separateAutomaticAndManualTransmissionCars();
-    mtcarsRegressionAnalyzerInstance$plotLinearModelBetweenMpgAndTransmissionUsingGgplot();
-    mtcarsRegressionAnalyzerInstance$plotLinearModelBetweenMpgAndTransmissionUsingBase();
-}
 
 plotResiduals <- function()
 {
     mtcarsRegressionAnalyzerInstance <- mtcarsRegressionAnalyzerFactory();
     mtcarsRegressionAnalyzerInstance$plotResidualsBetweenMpgAndTransmission();
 }
-
-#createBoxPlot()
-#createHistogramPlot()
-#test_interval <- testTInterval()
-#testLinearModelPlot()
